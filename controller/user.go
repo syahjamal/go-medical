b7de0d13cd183948e30badb84f930816f4a4d533
syahjamal <sulaimansyahjamal@gmail.com>
2020-11-06 10:20:47 +0700

    add controller func login

--- controller/user.go
+++ controller/user.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/syahjamal/go-medical/config"
 	"github.com/syahjamal/go-medical/models"
 	"golang.org/x/crypto/bcrypt"
@@ -46,4 +48,56 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(createdUser)
 
+}
+
+//Login function
+func Login(w http.ResponseWriter, r *http.Request) {
+	user := &models.User{}
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		var resp = map[string]interface{}{"status": false, "message": "Invalid request"}
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+	resp := FindOne(user.Email, user.Password)
+	json.NewEncoder(w).Encode(resp)
+}
+
+//FindOne function
+func FindOne(email, password string) map[string]interface{} {
+	user := &models.User{}
+
+	if err := db.Where("Email = ?", email).First(user).Error; err != nil {
+		var resp = map[string]interface{}{"status": false, "message": "Email address not found"}
+		return resp
+	}
+
+	expiresAt := time.Now().Add(time.Minute * 100000).Unix()
+
+	errf := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if errf != nil && errf == bcrypt.ErrMismatchedHashAndPassword { //Password does not match!
+		var resp = map[string]interface{}{"status": false, "message": "Invalid login credentials. Please try again"}
+		return resp
+	}
+
+	tk := &models.Token{
+		UserID: user.IDUser,
+		Name:   user.Username,
+		Email:  user.Email,
+		StandardClaims: &jwt.StandardClaims{
+			ExpiresAt: expiresAt,
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+
+	tokenString, error := token.SignedString([]byte("secret"))
+	if error != nil {
+		fmt.Println(error)
+	}
+
+	var resp = map[string]interface{}{"status": false, "message": "logged in"}
+	resp["token"] = tokenString //Store the token in the response
+	resp["user"] = user
+	return resp
 }
